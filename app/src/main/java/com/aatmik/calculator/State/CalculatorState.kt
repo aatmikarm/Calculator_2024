e5fe6590ffd043790ce6f29157cb282742156797
Aatmik <aatmikarm@gmail.com>
2024-09-13 14:21:34 +0530

    deleted all files

--- app/src/main/java/com/aatmik/calculator/State/CalculatorState.kt
+++
@@ -1,9 +0,0 @@
-package com.aatmik.calculator.State
-
-import com.aatmik.calculator.action.CalculatorOperation
-
-data class CalculatorState(
-    val number1: String = "",
-    val number2: String = "",
-    val operation: CalculatorOperation? = null
-)
