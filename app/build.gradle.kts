a786c29fa4a70b917e7df9c0f879ce7c3443d18d
Aatmik <aatmik@dozee.io>
2025-11-10 01:40:03 +0530

    new version 10 release on playstore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.aatmik.calculator"
         minSdk = 25
         targetSdk = 35
-        versionCode = 9
-        versionName = "9.0"
+        versionCode = 10
+        versionName = "10.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
